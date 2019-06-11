9187010bcd09c2ba30011c7452a74598de46569e
Plamen Mitev <pmitev89@gmail.com>
2019-06-11 23:35:56 +0200

    Add Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_151001) do
+ActiveRecord::Schema.define(version: 2019_06_11_125829) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2019_06_10_151001) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
