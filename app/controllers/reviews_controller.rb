486eea2df09e36a1938b88aceb5dbaa93e43c74c
Plamen Mitev <pmitev89@gmail.com>
2019-06-12 20:52:35 +0200

    Add review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
-  before_action :find_book
+  before_action :find_book 
+  before_action :find_review, only: [:edit, :update, :destroy]
+
   def new
     @review = Review.new
   end
@@ -7,13 +9,29 @@ class ReviewsController < ApplicationController
   def create
 		@review = Review.new(review_params)
 		@review.book_id = @book.id
-		@review.user_id = current_user
+		@review.user_id = current_user.id
 
     if @review.save
       redirect_to book_path(@book)
     else
       render 'new'
     end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end 
+
+  def destroy
+    @review.destroy
+    redirect_to book_path(@book)
   end
 
   private 
@@ -26,7 +44,9 @@ class ReviewsController < ApplicationController
       @book = Book.find(params[:book_id])
     end
 
-    
+    def find_review
+      @review = Review.find(params[:id])
+    end
 end
 
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :book
-  belongs_to :usr
+  belongs_to :user
 end
